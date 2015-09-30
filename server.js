d3f22f91e3439603eafca7fd775720e2c4f67802
ghosttim <adminix@bk.ru>
2015-09-30 08:46:09 +0300

    Переделан для отправки post запросов

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var http = require('http');
 
-http.get("http://www.google.com/index.html", function(res){
-	console.log("Got response: "+res.statusCode);
-}).on('error',function(e){
-	console.log("Got error:"+e.message);
+var options = {
+    hostname:'www.google.com',
+    port: 80,
+    path: '/upload',
+    mathod: 'POST'
+};
+
+var req = http.request(options, function(res){
+	console.log('STATUS: '+res.statusCode);
+	console.log('HEADERS: '+JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	res.on('data',function(chunk){
+		console.log('BODY: '+chunk);
+	});
 });
+
+req.on('error', function(e){
+	console.log('problem with request: '+e.message);
+});
+
+req.write('data\n');
+req.write('date\n');
+req.end();
